e1a5cdcc68d89edcaea989da4c3cb2c7408320dd
samriddha2011 <samsb.ishat@gmail.com>
2026-04-04 17:45:55 +0530

    not equal

---
+++ 5th lesson/not equal.py
@@ -0,0 +1,22 @@
+a = 10
+b = 12
+c = 12
+
+print(a != b)  # True
+print(b != c)  # False
+
+x = 'python'
+y = 'java'
+
+if x != y:
+    print("The strings are different")
+
+p = 4
+q = 5
+
+if (p == 1) != (q == 5):
+    print('hello')
+
+a = int(input("Enter a number: "))  
+if a%2 != 0 :
+    print("The number is odd")3
