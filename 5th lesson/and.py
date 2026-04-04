6d8d7ea2e85e76c5675002d96c544c858a9bce00
samriddha2011 <samsb.ishat@gmail.com>
2026-04-04 17:33:31 +0530

    and & or

---
+++ 5th lesson/and.py
@@ -0,0 +1,21 @@
+a = 10
+b = 12
+c = 0
+
+if a > 0 and b > 0 and c > 0:
+    print("All numbers are positive")
+else:
+    print("At least one number is not positive")
+
+x = 5
+y = -8
+z = 0
+if x > 0 or y > 0:
+    print("At least one number is positive")
+else:
+    print("Both numbers are not positive")
+
+if y > 0 or z > 0:
+    print("At least one number is positive")
+else:   
+    print("Both numbers are not positive")
