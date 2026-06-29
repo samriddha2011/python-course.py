dc20d84307eeba2519fd9cb56dd211b7d264b25d
samriddha2011 <samsb.ishat@gmail.com>
2026-06-29 11:17:19 +0530

    calculator

---
+++ 3.1 lesson/calculator.py
@@ -0,0 +1,31 @@
+def add(p, q):
+    return p + q
+
+def subtract(p, q):
+    return p - q
+
+def multiply(p, q):
+    return p * q
+
+def divide(p, q):
+    return p / q
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+choice = input("Enter choice(1/2/3/4):")
+num_1 = float(input("Enter first number: "))
+num_2 = float(input("Enter second number: "))
+
+if choice == '1':
+    print(num_1,"+",num_2,"=", add(num_1,num_2))
+elif choice == '2':
+    print(num_1,"-",num_2,"=", subtract(num_1,num_2))
+elif choice == '3':
+    print(num_1,"*",num_2,"=", multiply(num_1,num_2))
+elif choice == '4':
+    print(num_1,"/",num_2,"=", divide(num_1,num_2))
+else:
+    print("Invalid input")
